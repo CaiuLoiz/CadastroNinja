e4a03b64c0fd491c1d912662a30262e8aec1db9f
caiov1 <caioluiz1232@gmail.com>
2026-01-26 20:16:15 -0300

    chore: .gitignore adiciona as minhas credenciais do db

--- src/main/java/com/devcl/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/com/devcl/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -1,6 +1,10 @@
 package com.devcl.CadastroDeNinjas.Missoes;
 
+import com.devcl.CadastroDeNinjas.Ninjas.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+//JPA - É uma abstração para utilizar o BD, como queries
+//ORM - OBJECT RELATIONAL MAPPING
+//Tem ORM (Mapeamento de Objeto Relacional) - É responsável por fazer leitura nas Classes e criar tabelas
+public interface MissoesRepository extends JpaRepository<NinjaModel, Long> {
 }
--- src/main/java/com/devcl/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/com/devcl/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,7 @@
 package com.devcl.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import com.devcl.CadastroDeNinjas.Missoes.MissoesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<MissoesModel, Long> {
 }
