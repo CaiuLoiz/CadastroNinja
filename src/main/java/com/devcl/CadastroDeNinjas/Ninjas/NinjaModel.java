c3b15172b1d3269865acc1c2b482be9e115de6a7
caiov1 <caioluiz1232@gmail.com>
2026-01-26 09:32:18 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/com/devcl/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/devcl/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,10 +2,16 @@ package com.devcl.CadastroDeNinjas.Ninjas;
 
 import com.devcl.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //JPA (Java Persistence API)
 @Entity         //Transforma uma classe em uma entidade do BD
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
