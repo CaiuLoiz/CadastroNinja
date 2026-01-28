b28049cfeaaa7748b6bde0f07db50697cbfffa4c
caiov1 <caioluiz1232@gmail.com>
2026-01-28 11:17:39 -0300

    feat: Modifica o  controller com mock de dados

--- src/main/java/com/devcl/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/devcl/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,12 +1,20 @@
 package com.devcl.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
     @GetMapping("/ninja")
     public String message() { return "Página do Ninja"; }
+    @PostMapping("/adicionar")
+    public String criarNinja() { return "Ninja Criado"; }
+    @GetMapping("/mostrar")
+    public String mostrarNinjas() { return "Todos os Ninjas"; }
+    @GetMapping("/mostrar_id")
+    public String mostrarNinjasId() { return "Todos os Ids Ninja"; }
+    @PutMapping("/alterar")
+    public String alterarNinjaId() { return "Alterar Ninja"; }
+    @DeleteMapping("/deletar")
+    public String criarNinjaId() { return "Deletar Ninja"; }
 }
